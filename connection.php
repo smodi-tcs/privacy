82fbd85095776c3dcf33d1008da96a798fbfe9ae
Siddhant Modi <siddhant.modi@tcs.com>
2017-11-05 12:15:48 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,4 @@
+<?php
+    //open connection to mysql db
+    $connection = mysqli_connect("35.194.121.86","admin","123456","dghat_1to25") or die("Error " . mysqli_error($connection));
+?>
---
+++ game_data.php
@@ -0,0 +1,17 @@
+<?php
+    require('connection.php');
+    //fetch table rows from mysql db
+    $sql = "select version,isAvailable,install_count from t_game_data";
+    $result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+        $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($connection);
+?>
